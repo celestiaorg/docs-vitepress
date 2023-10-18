36896fcb01409feff352ea645e7a81c2e5c76188
jcstein <joshcs@celestia.org>
2023-10-18 18:39:14 +0000

    deploy: 2dde761cbb0a6f330cd9bac75dfeeb7e68483d27

---
+++ assets/developers_keplr.md.34b6f57c.lean.js
@@ -0,0 +1,37 @@
+import{v as l}from"./chunks/data.244b1a98.js";import{o as e,c as t,k as s,a as n,t as o,l as p,Q as a}from"./chunks/framework.25fa9d36.js";const c=a("",13),r={class:"vp-code-group vp-adaptive-theme"},E=a("",1),i={class:"blocks"},y={class:"language-js vp-adaptive-theme active"},d=s("button",{title:"Copy Code",class:"copy"},null,-1),_=s("span",{class:"lang"},"js",-1),h={class:"shiki github-dark vp-code-dark"},u=s("span",{class:"line"},[s("span",{style:{color:"#F97583"}},"import"),s("span",{style:{color:"#E1E4E8"}}," "),s("span",{style:{color:"#9ECBFF"}},"'@site/src/components/AddNetworkKeplr'")],-1),F=s("span",{class:"line"},null,-1),A=a("",1),C={class:"line"},m={style:{color:"#9ECBFF"}},q=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  chainName: 'Mocha testnet',")],-1),w=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  rpc: 'https://rpc-mocha.pops.one',")],-1),b=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  rest: 'https://api-mocha.pops.one/'")],-1),D=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"}`"),s("span",{style:{color:"#E1E4E8"}},"}")],-1),g=s("span",{class:"line"},null,-1),k=a("",1),T={class:"shiki github-light vp-code-light"},B=s("span",{class:"line"},[s("span",{style:{color:"#D73A49"}},"import"),s("span",{style:{color:"#24292E"}}," "),s("span",{style:{color:"#032F62"}},"'@site/src/components/AddNetworkKeplr'")],-1),f=s("span",{class:"line"},null,-1),I=a("",1),v={class:"line"},P={style:{color:"#032F62"}},S=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  chainName: 'Mocha testnet',")],-1),N=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  rpc: 'https://rpc-mocha.pops.one',")],-1),x=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  rest: 'https://api-mocha.pops.one/'")],-1),K=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"}`"),s("span",{style:{color:"#24292E"}},"}")],-1),V=s("span",{class:"line"},null,-1),R=a("",1),M={class:"language-js vp-adaptive-theme"},j=s("button",{title:"Copy Code",class:"copy"},null,-1),O=s("span",{class:"lang"},"js",-1),G={class:"shiki github-dark vp-code-dark"},H=s("span",{class:"line"},[s("span",{style:{color:"#F97583"}},"import"),s("span",{style:{color:"#E1E4E8"}}," "),s("span",{style:{color:"#9ECBFF"}},"'@site/src/components/AddNetworkKeplr'")],-1),Y=s("span",{class:"line"},null,-1),J=a("",1),W={class:"line"},Q={style:{color:"#9ECBFF"}},U=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  chainName: 'Arabica devnet',")],-1),z=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  rpc: 'https://consensus-full.celestia-arabica-10.com/',")],-1),L=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  rest: 'https://api.consensus.celestia-arabica-10.com/'")],-1),X=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"}`"),s("span",{style:{color:"#E1E4E8"}},"}")],-1),Z=s("span",{class:"line"},null,-1),$=a("",1),ss={class:"shiki github-light vp-code-light"},ns=s("span",{class:"line"},[s("span",{style:{color:"#D73A49"}},"import"),s("span",{style:{color:"#24292E"}}," "),s("span",{style:{color:"#032F62"}},"'@site/src/components/AddNetworkKeplr'")],-1),as=s("span",{class:"line"},null,-1),ls=a("",1),os={class:"line"},ps={style:{color:"#032F62"}},es=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  chainName: 'Arabica devnet',")],-1),ts=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  rpc: 'https://consensus-full.celestia-arabica-10.com/',")],-1),cs=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  rest: 'https://api.consensus.celestia-arabica-10.com/'")],-1),rs=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"}`"),s("span",{style:{color:"#24292E"}},"}")],-1),Es=s("span",{class:"line"},null,-1),is=a("",1),As=JSON.parse('{"title":"Keplr integration with Celestia","description":"How you can add Celestia network parameters to Keplr.","frontmatter":{"description":"How you can add Celestia network parameters to Keplr.","head":[["meta",{"name":"og:title","content":"Keplr integration with Celestia | Celestia Docs"},{"name":"og:description","content":false}]]},"headers":[],"relativePath":"developers/keplr.md","filePath":"developers/keplr.md","lastUpdated":1697647221000}'),ys={name:"developers/keplr.md"},Cs=Object.assign(ys,{setup(ds){return(_s,hs)=>(e(),t("div",null,[c,s("div",r,[E,s("div",i,[s("div",y,[d,_,s("pre",h,[s("code",null,[u,n(`
+`),F,n(`
+`),A,n(`
+`),s("span",C,[s("span",m,"  chainId: '"+o(p(l).chainId.mochaChainId)+"',",1)]),n(`
+`),q,n(`
+`),w,n(`
+`),b,n(`
+`),D,n(`
+`),g,n(`
+`),k])]),s("pre",T,[s("code",null,[B,n(`
+`),f,n(`
+`),I,n(`
+`),s("span",v,[s("span",P,"  chainId: '"+o(p(l).chainId.mochaChainId)+"',",1)]),n(`
+`),S,n(`
+`),N,n(`
+`),x,n(`
+`),K,n(`
+`),V,n(`
+`),R])])]),s("div",M,[j,O,s("pre",G,[s("code",null,[H,n(`
+`),Y,n(`
+`),J,n(`
+`),s("span",W,[s("span",Q,"  chainId: '"+o(p(l).chainId.arabicaChainId)+"',",1)]),n(`
+`),U,n(`
+`),z,n(`
+`),L,n(`
+`),X,n(`
+`),Z,n(`
+`),$])]),s("pre",ss,[s("code",null,[ns,n(`
+`),as,n(`
+`),ls,n(`
+`),s("span",os,[s("span",ps,"  chainId: '"+o(p(l).chainId.arabicaChainId)+"',",1)]),n(`
+`),es,n(`
+`),ts,n(`
+`),cs,n(`
+`),rs,n(`
+`),Es,n(`
+`),is])])])])])]))}});export{As as __pageData,Cs as default};
